62f767d552a5e735c4eec996099fdf0cf3817926
Manojit Nandi <51049709+hopeforfuture@users.noreply.github.com>
2019-07-01 13:58:39 +0530

    Test Change

--- addtocart.php
+++ addtocart.php
@@ -11,6 +11,7 @@ if(empty($getdata))
 }
 if(!array_key_exists('unique_id', $sessdata))
 {
+	/* Create unique ID */
 	$_SESSION['unique_id'] = time()."_".rand(10000,10000000);
 }
 if(array_key_exists('id', $getdata))
